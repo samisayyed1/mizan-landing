fdd8bf52bc0a58e67ab42e0515a5539ed1044f43
samisayyed1 <sami@tenetlabs.uk>
2026-05-06 17:48:44 +0530

    chore(landing): bump CURRENT_VERSION to 3.3.4

--- src/lib/downloads.ts
+++ src/lib/downloads.ts
@@ -18,7 +18,7 @@
  */
 
 /** Bump on each desktop release tag. Mirrors apps/tauri/tauri.conf.json. */
-export const CURRENT_VERSION = "3.3.2";
+export const CURRENT_VERSION = "3.3.4";
 
 const RELEASE_BASE = "https://github.com/samisayyed1/mizan-4/releases";
 
